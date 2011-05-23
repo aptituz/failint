fbb4aeb87265bdcd82f528383f8b66d6acdc9b77
Patrick Schoenfeld <patrick.schoenfeld@googlemail.com>
2011-05-23 09:22:38 +0200

    Fix export of Failint::Checks::Log

--- Failint/Checks/Log.pm
+++ Failint/Checks/Log.pm
@@ -2,18 +2,7 @@ package Failint::Checks::Log;
 
 use Exporter;
 @ISA = qw(Exporter);
-@EXPORT = qw( &debug &info &warning &error $fai_dir $class_dir
-$package_config_dir $debconf_dir $disk_config_dir $files_dir $hooks_dir
-$scripts_dir );
-
-our $fai_dir = $ARGV[0];
-our $class_dir = $fai_dir . "/class_dir";
-our $package_config_dir = $fai_dir . "/package_config";
-our $debconf_dir = $fai_dir . "/debconf";
-our $disk_config_dir = $fai_dir . "/disk_config";
-our $files_dir = $fai_dir . "/files";
-our $hooks_dir = $fai_dir . "/hooks";
-our $scripts_dir = $fai_dir . "/scripts";
+@EXPORT = qw( &debug &info &warning &error &verbose );
 
 sub debug {
     my $message = shift;
