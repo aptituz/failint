561972f458e64b74ceaa58e1ce948778ca193e38
Patrick Schoenfeld <patrick.schoenfeld@googlemail.com>
2011-05-23 16:19:26 +0200

    Add simple empty or missing dirs check

--- Failint/Checks/Vars.pm
+++ Failint/Checks/Vars.pm
@@ -6,7 +6,7 @@ use Exporter;
 $disk_config_dir $files_dir $hooks_dir $scripts_dir );
 
 our $fai_dir = $ARGV[0];
-our $class_dir = $fai_dir . "/class_dir";
+our $class_dir = $fai_dir . "/class";
 our $package_config_dir = $fai_dir . "/package_config";
 our $debconf_dir = $fai_dir . "/debconf";
 our $disk_config_dir = $fai_dir . "/disk_config";
