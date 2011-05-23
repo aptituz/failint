a5db72f656b6536f8f6afb33a8c40a29703948f9
Patrick Schoenfeld <patrick.schoenfeld@googlemail.com>
2011-05-23 08:57:24 +0200

    Various changes

---
+++ Failint/Checks/Helpers.pm
@@ -0,0 +1,57 @@
+package Failint::Checks::Helpers;
+
+use Failint::Checks::Log;
+use Scalar::Util qw(openhandle);
+
+require Exporter;
+our @ISA = qw(Exporter);
+our @EXPORT = qw( gte_fh get_interpreter check_script_for_syntax_errors );
+
+sub get_fh {
+    my $f = shift;
+
+    my $fh;
+    if (not openhandle($fh)) {
+        open($fh, "<", $f) or die "unable to open: $!";
+    } else {
+        $fh = $f;
+    }
+    return $fh;
+}     
+
+sub get_interpreter {
+    my $fh = get_fh(shift);
+    sysseek($fh, 0, SEEK_SET);
+    my $first_line = <$fh>;
+    if (not $first_line =~ /^#!(.*)/) {
+        return undef;
+    } else {
+        my $interpreter = $1;
+        $interpreter =~ s/^\s+|\s+$//g;
+        return $interpreter;
+    }
+}
+
+sub check_script_for_syntax_errors {
+    my $fname = shift;
+    my $interpreter = get_interpreter($fname);
+
+    # Test syntax of script for known interpreters
+    if ($interpreter =~ /sh/) {
+        if (-x $interpreter) {
+           system($interpreter, "-n", $fname);
+           my $rc = $? >> 8;
+           warning($fname,"", "shell script has syntax errors")
+            unless $rc == 0;
+        }
+    } elsif ($interpreter =~ /perl/) {
+        if (-x $interpreter) {
+            system($interpreter, "-c", $fname, ">/dev/null");
+            my $rc = $? >> 8;
+            warning($fname, "", "perl script has compilation errors")
+                unless $rc == 0;
+        }
+    }
+}
+
+1;
---
+++ Failint/Checks/Vars.pm
@@ -0,0 +1,17 @@
+package Failint::Checks::Vars;
+
+use Exporter;
+@ISA = qw(Exporter);
+@EXPORT = qw( $fai_dir $class_dir $package_config_dir $debconf_dir
+$disk_config_dir $files_dir $hooks_dir $scripts_dir );
+
+our $fai_dir = $ARGV[0];
+our $class_dir = $fai_dir . "/class_dir";
+our $package_config_dir = $fai_dir . "/package_config";
+our $debconf_dir = $fai_dir . "/debconf";
+our $disk_config_dir = $fai_dir . "/disk_config";
+our $files_dir = $fai_dir . "/files";
+our $hooks_dir = $fai_dir . "/hooks";
+our $scripts_dir = $fai_dir . "/scripts";
+
+1;
