ccfb28b702e9720196875b103c465146f7ba8ee7
Patrick Schoenfeld <patrick.schoenfeld@googlemail.com>
2011-05-25 08:51:44 +0200

    Add another check to typical-rootcmd-calls

--- Failint/Checks/Shell.pm
+++ Failint/Checks/Shell.pm
@@ -2,12 +2,13 @@ package Failint::Checks::Shell;
 
 require Exporter;
 our @ISA = qw(Exporter);
-our @EXPORT = qw($LEADIN $FCOPY_CMD $CP_CMD);
+our @EXPORT = qw($LEADIN $FCOPY_CMD $CP_CMD $ROOTCMD);
 
 our $LEADIN = qr'(?:(?:^|[`&;(\[|{])\s*|(?:if|then|do|while)\s+)';
 our $CMDARGS = qr'([^;\]}\|`)]+)';
 our $FCOPY_CMD = $LEADIN . qr'(?:fcopy) ' . $CMDARGS;
 our $CP_CMD = $LEADIN . qr'(?:cp)' . $CMDARGS;
 
+our $ROOTCMD = $LEADIN . qr'(?:\$ROOTCMD)';
 
 1;
